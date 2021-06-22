1a2491e2615643a260df7f5c5d45de40bbfe38f2
Vinicius Mendite <vinicius@sharbe.com.br>
2021-06-21 22:01:36 -0300

    create signin page, add assets

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  StatusBar
+} from 'react-native';
+
+import IllustrationImg from '../../assets/illustration.png';
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+import { styles } from './styles';
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <StatusBar 
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+      />
+      <Image source={IllustrationImg} resizeMode="stretch" style={styles.image} />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {'\n'}
+          suas jogatinas {'\n'}
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {'\n'}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon />
+      </View>
+    </View>
+  );
+}
