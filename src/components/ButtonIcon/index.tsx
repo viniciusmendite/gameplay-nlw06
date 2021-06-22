11b91c0439c37f859f7b8fd94223fd7fa6d791b7
Vinicius Mendite <vinicius@sharbe.com.br>
2021-06-21 22:10:21 -0300

    interface buttonicon

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Text, Image, View, TouchableOpacity } from 'react-native';
+import { Text, Image, View, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 
 import DiscordImg from '../../assets/discord.png';
 import { styles } from './styles';
 
-export function ButtonIcon() {
+interface IButtonIconProps extends TouchableOpacityProps {
+  title: string;
+}
+
+export function ButtonIcon({ title, ...rest }: IButtonIconProps) {
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} {...rest}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordImg} style={styles.icon} />
       </View>
       <Text style={styles.title}>
-        Entrar com Discord
+        {title}
       </Text>
     </TouchableOpacity>
   );
